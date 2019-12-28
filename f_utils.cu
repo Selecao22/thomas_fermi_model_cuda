c827b573597c9207cf39a6ffe44f148d4d176284
Nikita Yazikov <cour1337@gmail.com>
2019-12-28 12:18:35 +0300

    added init assumption computing at GPU

--- f_utils.cu
+++ f_utils.cu
@@ -2,19 +2,12 @@
 // Created by nikky on 26.12.2019.
 //
 
-#include <new>
 #include "f_utils.hpp"
+#include <cuda_runtime.h>
 
 double* create_physic_array(int size)
 {
-    double *array;
-
-    try {
-         array = new double [size];
-    }
-    catch (std::bad_alloc) {
-        return nullptr;
-    }
+    auto *array = (double*)calloc(size, sizeof(double));
     double point_step = 0.1;
 
     array[0] = 0.1;
@@ -26,5 +19,80 @@ double* create_physic_array(int size)
     }
 
     return array;
+}
+
+
+__global__
+void
+get_init_assumpiton_cuda
+(
+        double* dfi,
+        double* x,
+        double* fi,
+        double z,
+        double tet,
+        double r0,
+        double h,
+        double nu_0,
+        int n
+        )
+{
+    unsigned int block = blockDim.x * blockIdx.x + threadIdx.x;
+    double hi = block * h;
+
+    if (block < n)
+    {
+        fi[block] = (z / tet / r0) * (1.0 - 1.5 * pow(hi, 2.0) + 0.5 * pow(hi, 6.0)) - nu_0 * pow(hi, 2.0);
+        x[block] = hi;
+        dfi[block] = 0;
+    }
+
+}
+
+void
+get_init_assumption
+(
+        double **dfi,
+        double **x,
+        double **fi,
+        double z,
+        double tet,
+        double r0,
+        double h,
+        double nu_0,
+        int n
+        )
+{
+//    *fi = (double*)calloc(n, sizeof(double));
+    *fi = (double*)malloc(n * sizeof(double));
+    *x = (double*)calloc(n, sizeof(double));
+    *dfi = (double*)calloc(n, sizeof(double));
+    double* d_fi;
+    double* d_x;
+    double* d_dfi;
+
+    cudaMalloc(&d_fi, n * sizeof(double));
+    cudaMalloc(&d_x, n * sizeof(double));
+    cudaMalloc(&d_dfi, n * sizeof(double));
+
+    get_init_assumpiton_cuda<<<n / 1024, 1024>>>(
+            d_dfi,
+            d_x,
+            d_fi,
+            z,
+            tet,
+            r0,
+            h,
+            nu_0,
+            n
+    );
+
+    cudaMemcpy(*fi, d_fi, sizeof(double) * n, cudaMemcpyDeviceToHost);
+    cudaMemcpy(*x, d_x, sizeof(double) * n, cudaMemcpyDeviceToHost);
+    cudaMemcpy(*dfi, d_dfi, sizeof(double) * n, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_fi);
+    cudaFree(d_x);
+    cudaFree(d_dfi);
 }
 
--- f_utils.hpp
+++ f_utils.hpp
@@ -1,15 +1,29 @@
-#include <cuda_runtime.h>
+#include <cstdlib>
 
 // Physical constants and some thrash like a const iterations
 const int N_X = 20;
-const int N = 1000;
+const int N = 100000;
 const int POINT_NUMBER = 54;
 const double PI = 3.141592653589793;
 const double K = 36.75;
 const double H = 1.0/N;
-const double A = 107.0;
-const double Z = 47.0;
+const double A = 107.0; // tmp constant, must be as parameter
+const double Z = 47.0; // tmp constant, must be as parameter
 
 double* create_physic_array(int size);
+
+void
+get_init_assumption
+(
+        double **dfi,
+        double **x,
+        double **fi,
+        double z,
+        double tet,
+        double r0,
+        double h,
+        double nu_0,
+        int n
+);
 
 
